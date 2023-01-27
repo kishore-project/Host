2799ec84090f4aec327d1d4aa104c8d336e0dccb
kishore-project <kk90054@gmail.com>
2023-01-27 14:17:41 +0530

    first commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,22 @@
+export const WorkData = [
+    {
+        title: "Internal Assessment System ",
+        desc: "It is web-based application which is used to evaluate students internal assessments and maintain the academic records ",
+        tech: ["Asp.Net", " C#", " SQL Server", " CSS"],
+        gitlink: "//github.com/kishore-project/staff-s-review-students-performance.git",
+
+    },
+    {
+        title: "Calculator ",
+        desc: "I will use Java Swing components to create a simple calculator.Java Swing is an API for providing  elements to Java Programs.",
+        tech: ["java swing", "netbeans"],
+        gitlink: "//github.com/kishore-project/Calculator.git",
+
+    },
+    {
+        title: "Data Science",
+        desc: " Using Python- To analysing the current and historical trends in the stock market to make future buying and selling decisions.",
+        tech: ["Python", "Data Science", "Aanaconda"],
+        gitlink: "//github.com/kishore-project/-Stock-Market-Analysis-using-Python.git",
+    },
+];
